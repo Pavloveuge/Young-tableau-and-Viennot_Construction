3893d982e6939ae45e888b1e2958935d40a72378
Eugene <pavloveuge@gmail.com>
2021-12-27 14:30:08 +0300

    change algoritm

--- src/ViennotDiagram.cpp
+++ src/ViennotDiagram.cpp
@@ -1,70 +1,130 @@
 #include "ViennotDiagram.h"
+#include <QDebug>
 
 ViennotDiagram::ViennotDiagram(PYoungTable* PTable, QYoungTable* QTable, std::vector<int> permutation){
     this->PTable = PTable;
     this->QTable = QTable;
     this->permutation = permutation;
+    this->SizeDiagram = 0;
+}
+
+std::vector<std::vector<std::vector<int>>> ViennotDiagram::descendOne(std::vector<int> pt, std::vector<std::vector<int>> points, int n, bool isVert,
+                                                      std::vector<std::vector<int>> path, std::vector<std::vector<int>> nsp){
+    std::vector<std::vector<int>> newPts;
+    if (isVert){
+        path.push_back(pt);
+        for (int k = 0; k < (int) points.size(); k++){
+            if (pt[0] == points[k][0]){
+                if (k == 0){
+                    newPts = std::vector<std::vector<int>>(points.begin() + 1, points.end());
+                }
+                else{
+                    newPts = std::vector<std::vector<int>>(points.begin(), points.begin() + k);
+                    newPts.insert(newPts.end(), points.begin() + k + 1, points.end());
+                }
+                path.push_back(points[k]);
+                return descendOne(points[k], newPts, n, false, path, nsp);
+            }
+        }
+    }
+    else{
+        for (int i = 0; i < (int)points.size(); i++){
+            if (pt[1] > points[i][1]){
+                path.push_back({points[i][0], pt[1]});
+                path.push_back(points[i]);
+                newPts = std::vector<std::vector<int>>(points.begin(), points.begin() + i);
+                newPts.insert(newPts.end(), points.begin() + i + 1, points.end());
+                nsp.push_back({points[i][0], pt[1]});
+                return descendOne(points[i], newPts, n, false, path, nsp);
+            }
+        }
+        path.push_back({n + 1, pt[1]});
+        std::vector<std::vector<std::vector<int>>> res;
+        res.push_back(path);
+        res.push_back(points);
+        res.push_back(nsp);
+        return res;
+    }
+    std::vector<std::vector<std::vector<int>>> res;
+    return res;
 }
 
 
+Container1 ViennotDiagram::descendAll(std::vector<std::vector<int>> points, int n, int i,
+                                                      std::vector<std::vector<int>> nsp, std::vector<std::vector<std::vector<int>>> paths){
+    if (i > n or (int)points.size() == 0){
+        Container1 res(nsp, paths);
+        return res;
+    }
+    else{
+        std::vector<std::vector<std::vector<int>>> result = descendOne({i, n+ 1}, points, n);
+        if (result.size() != 0){
+            nsp.insert(nsp.end(), result[2].begin(), result[2].end());
+            paths.push_back(result[0]);
+            return descendAll(result[1], n, i + 1, nsp, paths);
+        }
+        else{
+            return descendAll(points, n, i + 1, nsp, paths);
+        }
+    }
+}
+
+std::vector<std::vector<std::vector<std::vector<int>>>> ViennotDiagram::ViennotHelperPaths(std::vector<std::vector<int>> points, int n, std::vector<std::vector<std::vector<int>>> syt1,
+                        std::vector<std::vector<std::vector<int>>> syt2, std::vector<std::vector<std::vector<std::vector<int>>>> paths){
+    if (points.size() == 0){
+        return paths;
+    }
+    else{
+        Container1 step = descendAll(points, n);
+        std::vector<std::vector<std::vector<int>>> tempPaths = step.second;
+        paths.push_back(tempPaths);
+        std::vector<std::vector<int>> syt1Part = {};
+        std::vector<std::vector<int>> syt2Part = {};
+        for (int i = 0; i < (int)tempPaths.size(); i++){
+            syt1Part.push_back(tempPaths[i][0]);
+        }
+        for (int i = 0; i < (int)tempPaths.size(); i++){
+            syt2Part.push_back(tempPaths[i][1]);
+        }
+        syt1.push_back(syt1Part);
+        syt2.push_back(syt2Part);
+
+        return ViennotHelperPaths(step.first, n, syt1, syt2, paths);
+    }
+
+}
+
 void ViennotDiagram::CreateImages(std::string dir_path){
     int SizeCell = 30;
-    QImage image(QSize(SizeCell * this->permutation.size(),SizeCell * this->permutation.size()),QImage::Format_RGB32);
+    this->SizeDiagram = SizeCell * (this->permutation.size() + 1);
+    QImage image(QSize(SizeDiagram,SizeDiagram),QImage::Format_RGB32);
     QPainter painter(&image);
-    painter.fillRect(QRectF(0,0,SizeCell * this->permutation.size(),SizeCell * this->permutation.size()),Qt::white);
+    painter.fillRect(QRectF(0,0,SizeDiagram,SizeDiagram),Qt::white);
     painter.setPen(QPen(Qt::black, 2));
-    for (int i = 0; i <= SizeCell * this->permutation.size(); i++){
-        if (i % SizeCell == 0){
-            painter.drawLine(i, 0, i, SizeCell * this->permutation.size());
-            painter.drawLine(0, i, SizeCell * this->permutation.size(), i);
-        }
-    }
-    std::vector<int> per = this->permutation;
-    std::vector<int> per_cp = this->permutation;
-    std::vector<int> index = {};
-    for (int i = 0; i < (int)this->permutation.size(); i++){
-        index.push_back(i);
-    }
-    for (int i = 0; i < QTable->CntRows(); i++){ // QTable->CntRows()
-        std::vector<int> skel(this->permutation.size(), this->permutation.size());
-        painter.setPen(QPen(Qt::GlobalColor(i + 7), 4));
-        for (int j = 0; j < (int)index.size(); j++){ // (int)per_cp.size()
-                int lenx = this->permutation.size() - index[j] - 1;
-                int leny = this->permutation.size() - per[index[j]];
-                for(int k = index[j] + 1; k < (int)per_cp.size(); k++){
-                    if (per[index[j]] > per_cp[k]){
-                        lenx = k - index[j];
-                        leny = this->permutation.size() - per[index[j]];
-                        if (lenx > 1){
-                            per_cp[k] = this->permutation.size() + 1;
-                        }
-                        skel[index[j] + lenx] = per[index[j]];
-                        break;
-                    }
-                }
-                int shift = this->permutation.size() - skel[index[j]];
-                leny = leny - shift;
-                if (shift == 0){
-                    painter.drawLine(index[j] * SizeCell, 0,
-                                     index[j] * SizeCell, 30);
-                }
-                painter.drawLine(index[j] * SizeCell, shift * SizeCell + 30,
-                                 index[j] * SizeCell, (shift + leny) * SizeCell + 30);
-                painter.drawLine(index[j] * SizeCell, (shift + leny) * SizeCell + 30,
-                                 (index[j] + lenx) * SizeCell, (shift + leny) * SizeCell + 30);
-                if (index[j] + lenx == (int)this->permutation.size() - 1){
-                    painter.drawLine((index[j] + lenx) * SizeCell, (shift + leny) * SizeCell + 30,
-                                     (index[j] + lenx + 1) * SizeCell, (shift + leny) * SizeCell + 30);
-                }
+    for (int i = 0; i <= this->SizeDiagram; i++){
+            if (i % SizeCell == 0){
+                painter.drawLine(i, 0, i, this->SizeDiagram);
+                painter.drawLine(0, i, this->SizeDiagram, i);
             }
+        }
+    std::vector<std::vector<int>> coor;
+        for (int i = 0; i < (int)this->permutation.size(); i++){
+            coor.push_back({i + 1, this->permutation[i]});
+        }
+    std::vector<std::vector<std::vector<std::vector<int>>>> ans = this->ViennotHelperPaths(coor, (int)this->permutation.size());
+    for (int i = 0; i < (int)ans.size(); i++){
+        painter.setPen(QPen(Qt::GlobalColor(i + 7), 4));
+        for (int j = 0; j < (int)ans[i].size(); j++){
+            int x_start = ans[i][j][0][0], y_start = ans[i][j][0][1];
+            for (int k = 0; k < (int)ans[i][j].size(); k++){
+                painter.drawLine(x_start * SizeCell, this->SizeDiagram - y_start * SizeCell,
+                                 ans[i][j][k][0] * SizeCell, this->SizeDiagram - ans[i][j][k][1] * SizeCell);
+                x_start = ans[i][j][k][0]; y_start = ans[i][j][k][1];
+            }
+        }
         image.save((dir_path + "/" + std::to_string(i) + ".png").c_str());
-        per = {};
-        index = QTable->AfterRow(i + 1);
-        for (int k = 0; k < (int)skel.size(); k++){
-            per.push_back(skel[k]);
-        }
-        per_cp = per;
-        }
+    }
+    //image.save((dir_path + "/" + std::to_string(i) + ".png").c_str());
 }
 
 
--- src/ViennotDiagram.h
+++ src/ViennotDiagram.h
@@ -1,16 +1,33 @@
 #ifndef VIENNOTDIAGRAM_H
 #define VIENNOTDIAGRAM_H
 #include <string>
+#include <unordered_set>
 #include "gif.h"
 #include "PYoungTable.h"
 #include "QYoungTable.h"
+
+struct Container1{
+    std::vector<std::vector<int>> first;
+    std::vector<std::vector<std::vector<int>>> second;
+    Container1(std::vector<std::vector<int>> b, std::vector<std::vector<std::vector<int>>> c){
+        first = b;
+        second = c;
+    }
+};
 
 class ViennotDiagram{
 public:
     ViennotDiagram(PYoungTable* PTable, QYoungTable* QTable, std::vector<int> permutation);
     void CreateImages(std::string dir_path);
     void CreateGif(std::string path_res);
+    std::vector<std::vector<std::vector<int>>> descendOne(std::vector<int> pt, std::vector<std::vector<int>> points, int n, bool isVert = true,
+                                                          std::vector<std::vector<int>> path = {}, std::vector<std::vector<int>> nsp = {});
+    Container1 descendAll(std::vector<std::vector<int>> points, int n, int i = 1,
+                                                          std::vector<std::vector<int>> nsp = {}, std::vector<std::vector<std::vector<int>>> paths = {});
+    std::vector<std::vector<std::vector<std::vector<int>>>> ViennotHelperPaths(std::vector<std::vector<int>> points, int n, std::vector<std::vector<std::vector<int>>> syt1 = {},
+                            std::vector<std::vector<std::vector<int>>> syt2 = {}, std::vector<std::vector<std::vector<std::vector<int>>>> paths = {});
 private:
+    int SizeDiagram;
     PYoungTable* PTable;
     QYoungTable* QTable;
     std::vector<int> permutation;
